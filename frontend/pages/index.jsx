56b048defdf26a0405f9ce22f3bdca2801e12ea8
e.mercado <admin@wayalabs.com>
2023-03-05 04:15:36 -0700

    update license and module

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,16 +1,18 @@
 import styles from "../styles/Home.module.css";
 import NftMinter from "../components/nftMinter";
-import contract from ".."
+import InstructionsComponent from "../components/InstructionsComponent";
 import nftAbi from "./abi/abi.json"
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <NftMinter contractAddress="0x828E499F0C302D9492f5DA138Fa84279b8C895b2" 
+
+      <InstructionsComponent> </InstructionsComponent>
+      <NftMinter contractAddress="0x828E499F0C302D9492f5DA138Fa84279b8C895b2" 
         abi={nftAbi} 
         imgSrc="https://nftstorage.link/ipfs/bafybeihzebqbqlmjbvdpunmrq7s733gh76avhonjmlhbov4gb2teibfng4"
         tokenUri="ipfs://bafybeihzebqbqlmjbvdpunmrq7s733gh76avhonjmlhbov4gb2teibfng4"/>
-        </main> 
+      </main>
     </div>
   );
 }
