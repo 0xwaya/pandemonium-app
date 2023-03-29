4c9c39162d974d6f5658c988ad1ec1eee7b716c3
e.mercado <admin@wayalabs.com>
2023-03-29 19:26:42 -0400

    commit to main

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,13 +1,11 @@
 import styles from "../styles/Home.module.css";
-import NftMinter from "../components/nftMinter";
 import InstructionsComponent from "../components/InstructionsComponent";
-import nftAbi from "./abi/abi.json"
+import nftAbi from "./abi/abi.json";
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
-
-      <InstructionsComponent> </InstructionsComponent>
+        <InstructionsComponent> </InstructionsComponent>
       </main>
     </div>
   );
